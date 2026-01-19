71c54a2a1e0bf59245e69f663814e72a5d21dc35
Qian_Junfan <qianjunfan0@gmail.com>
2026-01-19 21:18:40 +0900

    Jan 19, 2026
    mod build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,33 @@ pid_lower=$(echo "$pid" | tr '[:upper:]' '[:lower:]')
 
 read -p "Enter Problem Name: " pname
 
-read -p "Include IO optimization? (y/n): " io_opt
+echo "Select platform:"
+echo "        1) Codeforces"
+echo "        2) LeetCode"
+read -p "        Enter choice (1 or 2): " platform_choice
+
+case $platform_choice in
+1 | "codeforces" | "cf")
+  prefix="codeforces"
+  ;;
+2 | "leetcode" | "lc")
+  prefix="leetcode"
+  ;;
+*)
+  echo "        Invalid choice, defaulting to Codeforces"
+  prefix="codeforces"
+  ;;
+esac
+
+read -p "Include IO optimization? (y/n, default: y): " io_opt
+io_opt=${io_opt:-y}
 
 curr_date=$(date +"%b %d, %Y")
 
-cat <<EOF >"${pid_lower}.cpp"
-/* codeforces ${pid_lower} - ${pname}
+filename="${pid_lower}.cpp"
+
+cat <<EOF >"$filename"
+/* ${prefix} ${pid_lower} - ${pname}
  * ${curr_date}
  */
 #include <bits/stdc++.h>
@@ -26,26 +47,42 @@ int main(void)
 EOF
 
 if [[ "$io_opt" =~ ^[Yy]$ ]]; then
-  cat <<EOF >>"${pid_lower}.cpp"
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+  cat <<EOF >>"$filename"
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
 
 EOF
 fi
 
-cat <<EOF >>"${pid_lower}.cpp"
-    return 0;
+cat <<EOF >>"$filename"
+        return 0;
 }
 EOF
 
-echo "File ${pid_lower}.cpp created successfully!"
+echo "File ${filename} created successfully for ${prefix}!"
 
-read -p "Open with which editor? (vim/nvim/code/nano/none): " editor
+echo "Select editor to open file:"
+echo "        1) vim"
+echo "        2) nvim"
+echo "        3) VSCode (code)"
+echo "        4) nano"
+echo "        5) Don't open"
+read -p "        Enter choice (1-5): " editor_choice
 
-case $editor in
-vim) vim "${pid_lower}.cpp" ;;
-code) code "${pid_lower}.cpp" ;;
-nano) nano "${pid_lower}.cpp" ;;
-nvim) nvim "${pid_lower}.cpp" ;;
-*) echo "Done." ;;
+case $editor_choice in
+1 | "vim")
+  vim "$filename"
+  ;;
+2 | "nvim")
+  nvim "$filename"
+  ;;
+3 | "code" | "vscode")
+  code "$filename"
+  ;;
+4 | "nano")
+  nano "$filename"
+  ;;
+5 | "none" | *)
+  echo "        File created at: $PWD/$filename"
+  ;;
 esac
